04931eb79cdb35a265c25d62360c47e32137258d
Yuto Iwata <jyuchan415@gmail.com>
2019-12-01 22:40:46 +0900

    GameFrameCでの変数の設定変更.

--- src/com/o_char/pong/GameFrameC.java
+++ src/com/o_char/pong/GameFrameC.java
@@ -7,31 +7,34 @@ import javax.swing.JPanel;
 
 // PongClient向けゲーム画面用クラス
 public final class GameFrameC extends GameFrame {
-  private Container container;
-  private JPanel panel;
-
+  /**
+   * Constructor.
+   *
+   * @param n   設定する n.
+   * @param nid 設定する id.
+   * @param npc 設定する PongController.
+   */
   public GameFrameC(int n, int nid, PongController npc) {
     super(n, npc);
+    // パネルを設定して追加する.
+    JPanel panel = new JPanel();
+    panel.setLayout(null);
+    this.getContentPane().add(panel, BorderLayout.CENTER);
 
-    // パネル
-    this.panel = new JPanel();
-    this.panel.setLayout(null);
-
-    // コンテントペイン
-    this.container = this.getContentPane();
-    this.container.add(this.panel, BorderLayout.CENTER);
-
-    id = nid + 2;
-
+    this.id = nid + 2;
     this.init();
   }
 
+  /**
+   * 点を受け取る.
+   * @param s 受け取る情報が含まれた文字列.
+   */
   public synchronized void receivePoint(String s) {
     StringTokenizer st = new StringTokenizer(s, " ");
     st.nextToken();
     int nid = Integer.parseInt(st.nextToken());
     int newPoint = Integer.parseInt(st.nextToken());
-    if (nid != id) {
+    if (nid != this.id) {
       this.point[nid - 1] = newPoint;
     }
   }
