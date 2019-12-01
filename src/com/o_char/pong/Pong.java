20cde569aa031dbfac90a9c4736d6de93d0944a9
Yuto Iwata <jyuchan415@gmail.com>
2019-12-01 22:45:56 +0900

    Pong class の checkstyle 対応.

--- src/com/o_char/pong/Pong.java
+++ src/com/o_char/pong/Pong.java
@@ -1,7 +1,15 @@
 package com.o_char.pong;
 
-public class Pong {
-  public static void main(String[] args) {
+public final class Pong {
+  private Pong() {
+  }
+
+  /**
+   * main function.
+   *
+   * @param args 実行時の引数.
+   */
+  public static void main(final String[] args) {
     SelectFrame selectFrame = new SelectFrame();
     selectFrame.setVisible(true);
   }
